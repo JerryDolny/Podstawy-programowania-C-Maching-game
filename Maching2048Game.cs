05c243442e8e9e2444901a7c32e92ac8fb0c70b9
JerryDolny <jerrydolny@gmail.com>
2020-06-10 16:06:46 +0200

    First commit

---
+++ Maching2048Game.cs
@@ -0,0 +1,33 @@
+﻿namespace Kfl.Maching2048Game
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Run();
+        }
+    }
+
+    class Game
+    {
+        public ulong Score { get; private set; }
+        public ulong[,] Board { get; private set; }
+
+        private readonly int nRows;
+        private readonly int nCols;
+        private readonly Random random = new Random();
+
+        public Game()
+        {
+            this.Board = new ulong[4, 4];
+            this.nRows = this.Board.GetLength(0);
+            this.nCols = this.Board.GetLength(1);
+            this.Score = 0;
+        }
