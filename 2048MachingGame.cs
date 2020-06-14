a925a3f00bce8f8a52df921fbb58ae8796a4e00b
JerryDolny <jerrydolny@gmail.com>
2020-06-14 21:22:41 +0200

    Update 2048MachingGame.cs
    Update 2.2

--- 2048MachingGame.cs
+++ 2048MachingGame.cs
@@ -36,6 +36,57 @@
             //Test G 4
         }
 
+        public void Run()
+        {
+            bool hasUpdated = true;
+            do
+            {
+                if (hasUpdated)
+                {
+                    PutNewValue();
+                }
 
+                Display();
 
+                if (IsDead())
+                {
+                    using (new ColorOutput(ConsoleColor.Red))
+                    {
+                        Console.WriteLine("YOU ARE DEAD!!!");
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Use arrow keys to move the tiles. Press Ctrl-C to exit.");
+                ConsoleKeyInfo input = Console.ReadKey(true); 
+                Console.WriteLine(input.Key.ToString());
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        hasUpdated = Update(Direction.Up);
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        hasUpdated = Update(Direction.Down);
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        hasUpdated = Update(Direction.Left);
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        hasUpdated = Update(Direction.Right);
+                        break;
+
+                    default:
+                        hasUpdated = false;
+                        break;
+                }
+            }
+            while (true); 
+
+            Console.WriteLine("Press any key to quit...");
+            Console.Read();
+        }
         
